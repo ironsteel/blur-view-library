a3c54d3019620ed9cc5de1b50f28f50d95735633
Rangel Ivanov <rangelivanov88@gmail.com>
2014-02-04 12:27:49 +0200

    Removed unused method

--- src/eu/masconsult/blurview/MainActivity.java
+++ src/eu/masconsult/blurview/MainActivity.java
@@ -1,12 +1,9 @@
 
 package eu.masconsult.blurview;
 
-import java.util.ArrayList;
-import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import eu.masconsult.blurview.R;
 
 public class MainActivity extends Activity {
 
@@ -14,14 +11,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    List<String> getSamleData() {
-        ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < 200; i++) {
-            list.add("Sample " + i);
-        }
-        return list;
     }
 
     @Override
